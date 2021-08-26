524fbc370c637e4f94b09403eaa9231fc454be41
Pavel.K <pavelweb74@gmail.com>
2021-08-26 23:21:14 +0500

    remove console.log

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react'
-import './App.css'
-import SearchInput from './components/SearchInput/SearchInput'
-import FilterProducts from './components/FilterProducts/FilterProducts'
+import React, { Component } from "react";
+import "./App.css";
+import SearchInput from "./components/SearchInput/SearchInput";
+import FilterProducts from "./components/FilterProducts/FilterProducts";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      search: '',
+      search: "",
       isStock: false,
-    }
-    this.search = React.createRef()
+    };
+    this.search = React.createRef();
   }
 
-  onChangeSearch = (event) => {
+  onChangeSearch = event => {
     this.setState({
       search: event.target.value,
-    })
-  }
-  onChangeStock = (event) => {
+    });
+  };
+  onChangeStock = event => {
     this.setState({
       isStock: !this.state.isStock,
-    })
-  }
+    });
+  };
 
-  componentDidMount(){
-    this.search.current.focus()
-    console.log(this.search.current);
+  componentDidMount() {
+    this.search.current.focus();
   }
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <SearchInput
-          text='Показывать товары только в наличии'
+          text="Показывать товары только в наличии"
           search={this.state.search}
           isStock={this.state.isStock}
           onChangeSearch={this.onChangeSearch}
@@ -42,8 +41,8 @@ class App extends Component {
         />
         <FilterProducts products={this.props.products} search={this.state.search} isStock={this.state.isStock} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
