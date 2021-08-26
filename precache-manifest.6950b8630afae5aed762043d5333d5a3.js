06e4c65792420b8025bbb7277a016f2f7baa49dc
Pavel.K <pavelweb74@gmail.com>
2021-08-26 23:02:15 +0500

    Updates

---
+++ precache-manifest.6950b8630afae5aed762043d5333d5a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaca57066aaaa182d2f3dc265126eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c592545249053b29b0e6",
+    "url": "/static/css/main.88edc8ad.chunk.css"
+  },
+  {
+    "revision": "c821d6dee83e5c6ccb7f",
+    "url": "/static/js/2.41c38b4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41c38b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c592545249053b29b0e6",
+    "url": "/static/js/main.c27ea4a2.chunk.js"
+  },
+  {
+    "revision": "28614e8f00b90b658472",
+    "url": "/static/js/runtime-main.0f559a5e.js"
+  }
+]);
